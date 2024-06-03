96a4d7172fe9efa55cd2515c0ccf93c18dd2bbfe
YuYu1015 <44525760+whes1015@users.noreply.github.com>
2024-06-04 04:58:22 +0800

    Add files via upload

--- color_log/main.py
+++ color_log/main.py
@@ -1,53 +1,67 @@
 import logging
+import sys
 
 import pytz
 
+from cdps.config import Config
 import cdps.utils.logger
-from cdps.utils.logger import (CustomFormatter, CustomTimedRotatingFileHandler,
+from cdps.utils.logger import (CustomTimedRotatingFileHandler,
                                Log)
 
 
-class _ColoredFormatter(CustomFormatter):
+class PlainFormatter(logging.Formatter):
+    def format(self, record):
+        return super().format(record)
+
+
+class _ColoredFormatter(logging.Formatter):
     COLORS = {
         "DEBUG": "\033[0;36m",
         "INFO": "\033[0;32m",
         "WARNING": "\033[0;33m",
         "ERROR": "\033[0;31m",
-        "CRITICAL": "\033[0;35m",
         "RESET": "\033[0m"
     }
 
-    def __init__(self, fmt, datefmt, tz):
-        super().__init__(fmt, datefmt, tz)
+    def __init__(self, fmt, datefmt=None, tz=None):
+        super().__init__(fmt, datefmt)
+        self.tz = tz if tz else pytz.UTC
 
     def format(self, record):
         levelname = record.levelname
         message = super().format(record)
-        return f"{self.COLORS.get(levelname, self.COLORS['RESET'])}{message}{self.COLORS['RESET']}"
+        if hasattr(sys.stdout, 'isatty') and sys.stdout.isatty():
+            color = self.COLORS.get(levelname, self.COLORS["RESET"])
+            message = f"{color}{message}{self.COLORS['RESET']}"
+        return message
 
 
 def _setup_logger(self, *, url):
     logger = logging.getLogger("MyLogger")
     logger.handlers.clear()
     logger.setLevel("DEBUG")
+    logger.propagate = False
 
-    handler = CustomTimedRotatingFileHandler(
+    tz_utc_8 = pytz.timezone('Asia/Taipei')
+
+    file_handler = CustomTimedRotatingFileHandler(
         self.log_dir,
         when="midnight",
         interval=1,
         backupCount=5
     )
 
-    tz_utc_8 = pytz.timezone('Asia/Taipei')
-    formatter = _ColoredFormatter(
-        '[%(asctime)s][%(levelname)s] -> %(message)s', '%H:%M:%S', tz_utc_8
+    file_formatter = PlainFormatter(
+        '[%(asctime)s][%(levelname)s]: %(message)s', '%H:%M:%S'
     )
-    handler.setFormatter(formatter)
-
-    logger.addHandler(handler)
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
 
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
+    console_formatter = _ColoredFormatter(
+        '[%(asctime)s][%(levelname)s]: %(message)s', '%H:%M:%S', tz=tz_utc_8
+    )
+    console_handler.setFormatter(console_formatter)
     logger.addHandler(console_handler)
 
     self.logger = logger
@@ -56,6 +70,10 @@ def _setup_logger(self, *, url):
 cdps.utils.logger.Log.setup_logger = _setup_logger
 
 Log.reset_instance()
+log = Log()
+config = Config()
+log.logger.setLevel(config._data['log_level'])
+log.logger.day = config._data['log_save_days']
 
 
 def initialize(completion_event):
